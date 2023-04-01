f0b5ab7e15060dc0634a9a52ff07cc08b3af3a00
S4dallys <geometrygaming8@gmail.com>
2023-04-02 00:21:56 +0800

    minor adjustments to global.h

--- global.h
+++ global.h
@@ -22,47 +22,47 @@ typedef int boolean;
 
 typedef struct userTag 
 {
-    long long user_ID;
-    String10  password;
-    String30  address;
-    long long contact;
-    String20  name;
+    long long  user_ID;
+    String10   password;
+    String30   address;
+    long long  contact;
+    String20   name;
 
 } userType;
 
 typedef struct itemTag
 {
-    long long product_ID;
-    String20  name;
-    String15  category;
-    String30  description;
-    long long quantity_Available;
-    double    unit_Price;
-    long long seller_ID;
+    long long  product_ID;
+    String20   name;
+    String15   category;
+    String30   description;
+    long long  quantity_Available;
+    double     unit_Price;
+    long long  seller_ID;
 
 } itemType;
 
 typedef struct itemCountTag
 {
-    itemType item;
-    int      amount;
+    itemType  item;
+    int       amount;
 
 } itemCountType;
 
 typedef struct dateTag
 {
-    int month;
-    int day;
-    int year;
+    int  month;
+    int  day;
+    int  year;
 
 } dateType;
 
 typedef struct transactionTag
 {
-    dateType         date;
-    itemCountType    transaction_Log[6];
-    long long        buyer_ID;
-    long long        seller_ID;
-    float            amount;
+    dateType       date;
+    itemCountType  transaction_Log[6];
+    long long      buyer_ID;
+    long long      seller_ID;
+    float          amount;
 
 } transactionType;
