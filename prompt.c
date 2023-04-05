d43d0cbfd767a440753016e3808a5c89cda30754
plvzfq-rit <james_christian_ruiz@dlsu.edu.ph>
2023-04-05 15:28:19 +0800

    prompt doc

--- prompt.c
+++ prompt.c
@@ -5,6 +5,11 @@
 
 #endif
 
+/**
+ * clean_Input_Stream removes all characters that exist
+ * in the input stream up to and including the first new
+ * line it encounters.
+*/
 void 
 clean_Input_Stream ()
 {
@@ -17,6 +22,14 @@ clean_Input_Stream ()
     } while (garbage != '\n');
 }
 
+/**
+ * prompt_Int displays a provided prompt and accepts an
+ * integer from the user.
+ * @param prompt - a string that contains a message to the
+ *                 user
+ * @param address - a pointer to where the user input
+ *                  will be stored
+*/
 void
 prompt_Int (char   prompt[],
             int *  address)
@@ -28,6 +41,14 @@ prompt_Int (char   prompt[],
     scanf("%c", &garbage);
 }
 
+/**
+ * prompt_Long_Long displays a provided prompt and accepts 
+ * a number of data type long long from the user.
+ * @param prompt - a string that contains a message to the
+ *                 user
+ * @param address - a pointer to where the user input
+ *                  will be stored
+*/
 void
 prompt_Long_Long (char         prompt[],
                   long long *  address)
@@ -39,6 +60,14 @@ prompt_Long_Long (char         prompt[],
     scanf("%c", &garbage);
 }
 
+/**
+ * prompt_Double displays a provided prompt and accepts a
+ * double from the user.
+ * @param prompt - a string that contains a message to the
+ *                 user
+ * @param address - a pointer to where the user input
+ *                  will be stored
+*/
 void
 prompt_Double (char      prompt[],
                double *  address)
@@ -50,6 +79,14 @@ prompt_Double (char      prompt[],
     scanf("%c", &garbage);
 }
 
+/**
+ * prompt_Char displays a provided prompt and accepts a
+ * character from the user.
+ * @param prompt - a string that contains a message to the
+ *                 user
+ * @param address - a pointer to where the user input
+ *                  will be stored
+*/
 void
 prompt_Char (char    prompt[],
              char *  address)
@@ -61,6 +98,17 @@ prompt_Char (char    prompt[],
     scanf("%c", &garbage);
 }
 
+/**
+ * prompt_Int displays a provided prompt and accepts a
+ * a number of characters from the users and stores it
+ * as a string.
+ * @param prompt - a string that contains a message to the
+ *                 user
+ * @param address - a pointer to where the user input
+ *                  will be stored
+ * @param limit - the maximum number of characters that the
+ *                function will accept
+*/
 void
 prompt_StringN (char  prompt[],
                 char  address[],
@@ -89,4 +137,3 @@ prompt_StringN (char  prompt[],
 
     address[index] = 0;
 }
-
